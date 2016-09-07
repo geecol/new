f559fc6df4a65cccad9e0fadb6529d35c4d61d33
geecol <gucollin@gmail.com>
2016-09-07 10:32:38 +0200

    Fourth commit

--- testapp/Program.cs
+++ testapp/Program.cs
@@ -10,7 +10,10 @@ namespace testapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HELLO!!!");
+            Console.Write("Enter name: ");
+            var inp = Console.ReadLine();
+            Console.Write("Hello, " + inp + "!");
+            
             Console.ReadKey();
         }
     }
